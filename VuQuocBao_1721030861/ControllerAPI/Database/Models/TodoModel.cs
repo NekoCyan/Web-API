87d2ed18c2bdbae452a6227f6a74ff05869efbbb
NekoCyan <cyanhijirikawa88@gmail.com>
2024-08-27 04:33:45 +0700

    feat; Controller API.

---
+++ VuQuocBao_1721030861/ControllerAPI/Database/Models/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControllerAPI.Database.Models
+{
+    public class TodoModel
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
---
+++ VuQuocBao_1721030861/ControllerAPI/Database/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using ControllerAPI.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControllerAPI.Database
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoModel> TodoItems { get; set; } = null!;
+    }
+}
