91802f019a25a2b9d9919ab27e74e3932f651ee1
NekoCyan <cyanhijirikawa88@gmail.com>
2024-08-27 02:13:42 +0700

    feat: Minimal API example.
    https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio

---
+++ VuQuocBao_1721030861/Minimal-API/Database/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_API.Database.Models;
+
+namespace Minimal_API.Database
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
