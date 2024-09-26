081446d8902fc74acd31cc9aea532e8758423e24
NekoCyan <cyanhijirikawa88@gmail.com>
2024-09-27 04:41:00 +0700

    feat: connection settings.

--- VuQuocBao_1721030861/ControllerAPI_1721030861/Program.cs
+++ VuQuocBao_1721030861/ControllerAPI_1721030861/Program.cs
@@ -5,7 +5,10 @@ using static ControllerAPI_1721030861.Utils.WebBuilderUtils;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.DbContextRegister<GeneralCatalogContext>("GeneralCatalog");
+builder
+    .DbContextRegister<GeneralCatalogContext>("GeneralCatalog")
+    .DbContextRegister<APITeachingContext>("APITeaching");
+
 builder.AutoScoped();
 
 builder.Services.AddControllers();
