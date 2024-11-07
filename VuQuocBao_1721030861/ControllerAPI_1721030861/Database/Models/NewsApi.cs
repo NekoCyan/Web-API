119857814246c4bf5685800dddd10f4eaaf306b3
NekoCyan <cyanhijirikawa88@gmail.com>
2024-11-07 16:24:12 +0700

    feat: clean up.

---
+++ VuQuocBao_1721030861/ControllerAPI_1721030861/Database/Models/NewsApi.cs
@@ -0,0 +1,43 @@
+﻿namespace ControllerAPI_1721030861.Database.Models;
+
+public partial class NewsApi
+{
+    public int Id { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Detail { get; set; } = null!;
+
+    public string ImageFile { get; set; } = null!;
+
+    public DateOnly PublishDate { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual CategoryApi Category { get; set; } = null!;
+
+    public virtual UserApi User { get; set; } = null!;
+}
+
+public partial class NewsApiDTO
+{
+    public int Id { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Detail { get; set; } = null!;
+
+    public string ImageFile { get; set; } = null!;
+
+    public DateOnly PublishDate { get; set; }
+
+    public int UserId { get; set; }
+}
